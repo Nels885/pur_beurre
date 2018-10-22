8ee1d04421bf7c82233af05b775f9e3947ad7336
Nels885 <voirin.lionel@gmail.com>
2018-10-22 18:30:51 +0200

    Add models of Pur beurre database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    nutriscore = models.CharField(max_length=10)
+    url = models.URLField()
+    picture = models.URLField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    products = models.ManyToManyField(Product, related_name='categories', blank=True)
+
+
+class Backup(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
