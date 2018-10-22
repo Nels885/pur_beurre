7e54fdb93fa1085df213c54fd02bf2c61db314ff
Nels885 <voirin.lionel@gmail.com>
2018-10-22 16:28:02 +0200

    add the registration form

--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,13 @@
 from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+
+class RegistrationFrom(UserCreationForm):
+
+    email = forms.EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email',)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
+from .forms import RegistrationFrom
 
 # Create your views here.
 
@@ -22,5 +23,14 @@ def food(request):
 
 
 def account(request):
-    return render(request, 'app/account.html')
+    if request.method == 'POST':
+        form = RegistrationFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/app/login/')
+        context = {'form': form}
+    else:
+        form = RegistrationFrom()
+        context = {'form': form}
+    return render(request, 'app/account.html', context)
 
