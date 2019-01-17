afbbf5f2cd47e608069cb310c798e89894749a98
Nels885 <voirin.lionel@gmail.com>
2019-01-17 12:55:54 +0100

    Add db_database_url

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +31,6 @@ else:
     DEBUG = True
 
 ALLOWED_HOSTS = ['django-pur-beurre.herokuapp.com']
-
 
 # Application definition
 
@@ -156,3 +157,6 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
